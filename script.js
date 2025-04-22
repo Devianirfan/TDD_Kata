5d7ddd5c1ffdb44ca5a7eeb6f6a91b55e2938195
Irfan Devian <devianirfan@gmail.com>
2025-04-22 17:24:49 +0200

    clean code

--- script.js
+++ script.js
@@ -3,27 +3,22 @@
 function fizzBuzz(n) {
   let result = "";
 
-  // Count occurrences of "Fizz" (divisible by 3 and contains 3)
   if (n % 3 === 0) {
     result += "Fizz";
   }
-  result += n.toString().split("").filter((digit) => digit === "3").map(() => "Fizz").join("");
+  result += n.toString().split("").filter((num) => num === "3").map(() => "Fizz").join("");
 
-  // Count occurrences of "Buzz" (divisible by 5 and contains 5)
   if (n % 5 === 0) {
     result += "Buzz";
   }
-  result += n.toString().split("").filter((digit) => digit === "5").map(() => "Buzz").join("");
+  result += n.toString().split("").filter((num) => num === "5").map(() => "Buzz").join("");
 
-  // Return the result or the number if no Fizz/Buzz
   return result || n;
 
 }
 
-// Export the function for testing
 module.exports = fizzBuzz;
 
-// Uncomment this to run the program manually
 // for (let i = 1; i <= 100; i++) {
 //   console.log(fizzBuzz(i));
 // }
--- script.test.js
+++ script.test.js
@@ -1,21 +1,21 @@
 const fizzBuzz = require('./script');
 
 test('returns Fizz for multiples of 3 or numbers containing 3', () => {
-  expect(fizzBuzz(3)).toBe("FizzFizz"); // Divisible by 3 and contains 3
-  expect(fizzBuzz(6)).toBe("Fizz"); // Divisible by 3
-  expect(fizzBuzz(13)).toBe("Fizz"); // Contains 3
+  expect(fizzBuzz(3)).toBe("FizzFizz");
+  expect(fizzBuzz(6)).toBe("Fizz");
+  expect(fizzBuzz(13)).toBe("Fizz");
 });
 
 test('returns Buzz for multiples of 5 or numbers containing 5', () => {
-  expect(fizzBuzz(5)).toBe("BuzzBuzz"); // Divisible by 5 and contains 5
-  expect(fizzBuzz(10)).toBe("Buzz"); // Divisible by 5
-  expect(fizzBuzz(52)).toBe("Buzz"); // Contains 5
+  expect(fizzBuzz(5)).toBe("BuzzBuzz");
+  expect(fizzBuzz(10)).toBe("Buzz");
+  expect(fizzBuzz(52)).toBe("Buzz");
 });
 
 test('returns FizzBuzz for numbers satisfying both Fizz and Buzz rules', () => {
-  expect(fizzBuzz(15)).toBe("FizzBuzzBuzz"); // Divisible by 3 and 5, contains 5
-  expect(fizzBuzz(53)).toBe("FizzBuzz"); // Contains 3 and 5
-  expect(fizzBuzz(35)).toBe("FizzBuzzBuzz"); // Contains 3 and 5, divisible by 5
+  expect(fizzBuzz(15)).toBe("FizzBuzzBuzz");
+  expect(fizzBuzz(53)).toBe("FizzBuzz");
+  expect(fizzBuzz(35)).toBe("FizzBuzzBuzz");
 });
 
 test('returns the number for non-FizzBuzz numbers', () => {
